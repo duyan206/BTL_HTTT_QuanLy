a2cf65cd60b4eaff95b4921c1ef4d4d8dc44227d
duy an <leduyank34t1@gmail.com>
2026-07-05 17:24:41 +0700

    Upload toan bo code BTL

--- Data/QlThuChiNoiBoContext.cs
+++ Data/QlThuChiNoiBoContext.cs
@@ -38,10 +38,7 @@ public partial class QlThuChiNoiBoContext : DbContext
 
     public virtual DbSet<ThongBao> ThongBaos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-Q7UUVA9; Database=QL_ThuChiNoiBo; Trusted_Connection=True; TrustServerCertificate=True;");
-
+   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CauHinhLuongDuyet>(entity =>
